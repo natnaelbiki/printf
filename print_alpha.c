fd5a9d5acfa9bf52bde22120e50e72aced4270b7
natnaelbiki <natnaelbiki@gmail.com>
2023-01-24 07:03:26 -0800

    second commit

---
+++ print_alpha.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_string - loops through a string and prints every char
+ * @l: va_list arguments
+ * @f: pointer to the struct flag
+ * Return: number of chars
+ */
+int print_string(va_list l, flags_t *f)
+{
+	char *s = va_arg(l, char *);
+
+	(void)f;
+	if (!s)
+		s = "(null)";
+	return (_puts(s));
+}
+
+/**
+ * print_char - prints a character
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * Return: number of char printed
+ */
+int _print_char(va_list l, flags_t *f)
+{
+	(void)f;
+	_putchar(va_arg(l, int));
+	return (1);
+}
